820d20b8d4dafde810582ed6b8d5e32bdadb7019
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-03-13 11:11:16 -0300

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,48 @@
+def jogar_jokenpo():
+    #Definição das variáveis#
+    total = 3
+    turno = 1
+    mao_bot = 0
+
+    print("-------------------------------")
+    print("\nBem vindo ao Jokenpo!\n")
+    print("-------------------------------")
+
+    for turno in range(1,total + 1):
+        print(f"\nRodada {turno} de {total}")
+        print("\nPedra = 1, papel = 2 e tesoura = 3")
+        entrada = int(input("Digite o número da sua escolha : "))
+
+        if(entrada == 1):
+            entrada == 1
+            print("\nVocê escolheu pedra")
+            mao_bot = mao_bot + 1
+        elif entrada == 2:
+            entrada == 2
+            print("\nVocê escolheu papel")
+            mao_bot = mao_bot + 1
+        else:
+            entrada == 3
+            print("\nVocê escolheu tesoura")
+            mao_bot = mao_bot + 1
+
+        if(mao_bot == 1 and entrada == 1):
+            print(f"\nEmpatamos o turno {turno}")
+        elif mao_bot == 2 and entrada == 1:
+            print(f"\nGanhei o turno {turno}")
+        elif mao_bot == 3 and entrada == 1:
+            print(f"\nPerdi o turno {turno}")
+        elif mao_bot == 1 and entrada == 2:
+            print(f"\nPerdi o turno {turno}")
+        elif mao_bot == 2 and entrada == 2:
+            print(f"\nEmpatamos o turno {turno}")
+        elif mao_bot == 3 and entrada == 2:
+            print(f"\nGanhei o turno {turno}")
+        elif mao_bot == 1 and entrada == 3:
+            print(f"\nGanhei o turno {turno}")
+        elif mao_bot == 2 and entrada == 3:
+            print(f"\nPerdi o turno {turno}")
+        elif mao_bot == 3 and entrada == 3:
+            print(f"\nEmpatamos o turno {turno}")
+
+    print("\nFim de jogo")
